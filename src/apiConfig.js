47d4cc68f4697792cecef9d7bf392aec4296f307
MacBook Pro5 <macbookpro5@MacBooks-MacBook-Pro-99.local>
2018-12-08 16:33:12 -0500

    links again

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://arcane-woodland-92648.herokuapp.com/:4741',
+  production: 'https://arcane-woodland-92648.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
